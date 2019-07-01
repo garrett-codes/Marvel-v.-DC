eb90023c9457a62dff2e9d059855868d8a53bb61
Garrett Halstein <garrett.halstein@emory.edu>
2019-06-30 21:05:22 -0400

    Added initial CRUD to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,35 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+  	#byebug
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.create(user_params)
+  	redirect_to user_path(@user)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to users_path
+  end
+
+  def user_params
+    return params.require(:user).permit(:name, wins: 0, losses: 0)
   end
 end
